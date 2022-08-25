c4b82d69e0458c4b719d092b6d4b2a78002936d7
Sadikshya Pokharel <psaishya@gmail.com>
2022-08-25 07:39:29 +0545

    components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
 
+import About from './components/About';
+import Navbar from './components/Navbar';
+import Textform from './components/Textform';
+import Alert from './components/Alert';
+import React, {useState} from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+
+document.body.style.backgroundColor='rgb(219, 220, 196)';
+document.body.style.color='black';
 function App() {
+  
+  //aru components : light: rgb(183, 186, 140)
+  const[mode,setMode]=useState('light');
+  const[alert,setAlert]=useState(null);
+  const changemode=()=>{
+    if(mode==='light')
+    {
+      setMode('dark');
+      
+      document.body.style.backgroundColor='rgb(46, 40, 45)';
+      document.body.style.color='white';
+      
+      setalertfunc("Dark mode has been enabled","success");
+      
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor='rgb(219, 220, 196)';
+      document.body.style.color='black';
+      setalertfunc("Dark mode has been disabled","success");
+  }
+  }
+
+  const setalertfunc=(message,type)=>{
+    setAlert({
+      msg:message,
+      type:type
+    }) 
+    setTimeout(() => {
+      setAlert(null)
+    }, 1500);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>  
+      <Navbar title="Mytexts" mode={mode} changemode={changemode}/>
+      <Alert alert={alert} /> 
+
+      <Router>
+        <Routes>
+          <Route 
+              path="/about" 
+              element={ <About mode={mode}/>}>
+          </Route>
+        
+          <Route 
+              path="/textform"
+              element={<div className="container">
+                <Textform heading="Enter your text here" mode={mode}  setalertfunc={setalertfunc}/>
+                </div>}>
+          </Route>
+        </Routes>
+      </Router>
+  
+
+    
+    
+    
+    </>
   );
 }
 
